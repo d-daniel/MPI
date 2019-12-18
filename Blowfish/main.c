940863ddf860e4d3c638784b4c9e7ee5766b26d5
d-daniel <33848208+d-daniel@users.noreply.github.com>
2019-12-18 09:58:21 -0600

    Create main.c

---
+++ Blowfish/main.c
@@ -0,0 +1,127 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include <mpi.h>
+
+#include "blowfish.h"
+#include "walltime.h"
+
+int main(int argc, char** argv) {
+
+  size_t i;
+  uint32_t lhs, rhs;
+  uint64_t N, fsize;
+  double t0, t1, t2, t3;
+  FILE* fp;
+  char fname[64];
+  char key[56]; // 448 bits key
+  uint64_t* p_array = NULL;
+  uint64_t* ans = NULL;
+
+  int size, rank;
+  MPI_Init(&argc, &argv);
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+  if (rank == 0) {
+    if (argc < 4) {
+      printf( "  Options:\n" );
+      printf( "  -file (-f): name of input file\n" );
+      printf( "  -key  (-k): encryption key\n" );
+      exit(1);
+    }
+  }
+  else {
+    if (argc < 4)
+      exit(1);
+  }
+
+  for (i = 0; i < argc; i++) {
+    if ((strcmp(argv[i], "-f") == 0) || (strcmp(argv[i], "-file") == 0)) {
+      strcpy(fname, argv[++i]);
+      //printf( "  User file: %s\n", fname);
+    }
+    else if ((strcmp(argv[i], "-k") == 0) || (strcmp(argv[i], "-key") == 0)) {
+      strcpy(key, argv[++i]);
+      //printf( "  User %ld bytes key: %s\n", strlen(key), key);
+    }
+  }
+
+  t0 = walltime();
+  fp = fopen(fname, "r");
+  fseek(fp, 0L, SEEK_END);
+  fsize = ftell(fp);
+  rewind(fp);
+  N = (fsize/8) + ((fsize/8) == 0 ? 0 : 1);
+
+  if (rank == 0) {
+    printf ("File size: %ld bytes split into %ld blocks of 64 bits\n", fsize, N);
+    p_array = (uint64_t*)malloc(N*sizeof(uint64_t));
+    fread(p_array, 8, N, fp);
+  }
+
+  fclose(fp);
+
+  BLOWFISH_CTX ctx;
+  Blowfish_Init(&ctx, (unsigned char*)key, strlen(key));
+
+  int* counts = (int*)malloc(size*sizeof(int));
+  int* displs = (int*)malloc(size*sizeof(int));
+
+  uint64_t sum = 0;
+  uint64_t remainder = N % size;
+
+  for (i = 0; i < size; i++) {
+    counts[i] = N/size;
+    if (remainder > 0) {
+      counts[i]++;
+      remainder--;
+    }
+    displs[i] = sum;
+    sum += counts[i];
+  }
+
+  uint64_t* buff = (uint64_t*)malloc(counts[rank]*sizeof(uint64_t));
+
+  t1 = walltime();
+  
+  MPI_Scatterv(p_array, counts, displs, MPI_UINT64_T, buff, counts[rank], MPI_UINT64_T, 0, MPI_COMM_WORLD);
+
+  for (i = 0; i < counts[rank]; i++) {
+    lhs = (uint32_t)((buff[i] & 0xFFFFFFFF00000000LL) >> 32);
+    rhs = (uint32_t) (buff[i] & 0x00000000FFFFFFFFLL);
+    Blowfish_Encrypt(&ctx, &lhs, &rhs);
+    Blowfish_Decrypt(&ctx, &lhs, &rhs);
+    buff[i] = (((uint64_t)lhs) << 32 | rhs);
+  }
+
+  if (rank == 0) ans = (uint64_t*)malloc(N*sizeof(uint64_t));
+
+  MPI_Gatherv(buff, counts[rank], MPI_UINT64_T, ans, counts, displs, MPI_UINT64_T, 0, MPI_COMM_WORLD);
+
+  t2 = walltime();
+
+  if (rank == 0) {
+    if (memcmp(p_array, ans, N) == 0) printf("PASSED\n");
+    else printf("FAILED\n");
+  }
+
+  free(p_array);
+  free(counts);
+  free(displs);
+  free(buff);
+  free(ans);
+
+  t3 = walltime();
+
+  if (rank == 0) {
+      printf("  Execution times:\n");
+      printf("  Initialization: %fs\n", t1 - t0);
+      printf("  Computation:    %fs\n", t2 - t1);
+      printf("  Total:          %fs\n", t3 - t0);
+  }
+  
+  MPI_Finalize();
+  return 0;
+}
